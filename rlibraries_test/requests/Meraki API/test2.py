4af3076494d99620d251c43a60f3304ad9453ada
PIVI Maxime <maximepivi@gmail.com>
2024-03-29 13:40:52 +0000

    exemple d'acces au call API de Meraki dans le fichier test2 est bien car jai ajourer la logique de verification de la presence de la clef dans une liste avan de iterer la liste

--- rlibraries_test/requests/Meraki API/test2.py
+++ rlibraries_test/requests/Meraki API/test2.py
@@ -2,8 +2,11 @@ import meraki
 from pprint import pprint
 
 API_KEY = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+try:
+    dashboard = meraki.DashboardAPI (API_KEY)
+except Exception as e:
+    print ("Error: Invalid API Key - " + e)
 
-dashboard = meraki.DashboardAPI (API_KEY)
 
 org = dashboard.organizations.getOrganizations()
 for org_item in org:
@@ -18,4 +21,20 @@ networks = dashboard.organizations.getOrganizationNetworks(orgid)
 for network_item in networks:
     break
 netid = network_item['id']
-print ("The network name is" + network_item['name'] + " and it's ID is "+ network_item['netid'])
+print ("The network name is: " + network_item['name'] + " and it's ID is "+ network_item['id'])
+
+devices = dashboard.networks.getNetworkDevices(netid)
+
+for device_item in devices:
+    if 'lanIP' in device_item and device_item['lanIP'] == "10.10.10.131":
+        break
+
+        
+deviceSerial = device_item['serial']
+
+print (deviceSerial)
+
+#clients = dashboard.networks.getNetworkClients(netid)
+#clients = dashboard.networks.getNetworkClientsOverview(netid)
+#pprint (clients)
+
